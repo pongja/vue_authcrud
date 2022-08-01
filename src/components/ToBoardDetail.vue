6d0fd89f5f2940e0b04dba75551511fbdf881c53
pongja <58586037+pongja@users.noreply.github.com>
2022-08-01 17:15:11 +0900

    boarddetail 안돼서 재백업

--- src/components/ToBoardDetail.vue
+++ src/components/ToBoardDetail.vue
@@ -5,32 +5,32 @@
        <div class="line-height">
           <div class="display_f">
             <div>제목</div>
-            <p>{{$route.params.title}}</p>
+            <p>{{form.title}}</p>
           </div>
           <hr/>
           <div class="display_f">
             <div>회사</div>
-            <p> {{$route.params.company}}</p>
+            <p> {{form.company}}</p>
           </div>
           <hr/>
           <div class="display_f">
             <div>회사주소</div>
-            <p> {{$route.params.company_url}}</p>
+            <p> {{form.company_url}}</p>
           </div>
           <hr/>
           <div class="display_f">
             <div>지역</div>
-            <p>{{$route.params.location}}</p>
+            <p>{{form.location}}</p>
           </div>
           <hr/>
           <div class="display_f">
             <div>내용</div>
-            <div class="write_widt"><p>{{$route.params.description}}</p></div>
+            <div class="write_widt"><p>{{form.description}}</p></div>
           </div>
           <hr/>
           <div class="display_f">
             <div>날짜</div>
-            <p>{{$route.params.date_posted}}</p>
+            <p>{{form.date_posted}}</p>
           </div>
        </div>
 </template>
@@ -39,9 +39,7 @@
 import { ref } from "vue";
 import axios from 'axios';
 
-const props = defineProps({
-  id: [String, Number],
-});
+
 const form = ref({
         company: null,
         company_url: null,
@@ -53,7 +51,7 @@ const form = ref({
 axios.get("http://127.0.0.1:8000/jobs/get/{id}")
 .then((res)=>{
   console.log(res.data)
-  form.value = props.id
+  form.value = res.data;
 })
 </script>
 
