03e2f53edd4a5111c640e4383c69da1b096d66aa
pongja <58586037+pongja@users.noreply.github.com>
2022-08-01 11:22:20 +0900

    board 페이지 수정

--- src/components/ToBoardDetail.vue
+++ src/components/ToBoardDetail.vue
@@ -2,34 +2,37 @@
   <div>
     게시글 상세페이지
   </div>
-   <table>
-        <tr>
-            <td>제목</td>
-            <td><input type="text" class="input_color" id="title" name="title" v-model="form.title"></td>
-        </tr>
-        <tr>
-            <td>회사</td>
-            <td><input type="text" class="input_color" name="company" id="company" v-model="params.company"></td>
-        </tr>
-        <tr>
-            <td>회사주소</td>
-            <td><input type="text" class="input_color" name="company_url" id="company_url" v-model="params.company_url"></td>
-        </tr>
-        <tr>
-            <td>지역</td>
-            <td><input type="text" class="input_color" name="location"  id="location" v-model="params.location"></td>
-        </tr>
-        <tr>
-            <td>내용</td>
-            <td>
-                <textarea name="description"  id="description" v-model="params.description" ></textarea>
-            </td>
-        </tr>
-          <tr>
-            <td>오늘날짜</td>
-            <td><input type="text" class="input_color" name="location"  id="date_posted" v-model="params.date_posted"></td>
-        </tr>
-    </table>
+       <div v-for="params in form" :key="params" class="line-height">
+          <div class="display_f">
+            <div>제목</div>
+            <p>{{params.title}}``</p>
+          </div>
+          <hr/>
+          <div class="display_f">
+            <div>회사</div>
+            <p> {{params.company}}</p>
+          </div>
+          <hr/>
+          <div class="display_f">
+            <div>회사주소</div>
+            <p> {{params.company_url}}</p>
+          </div>
+          <hr/>
+          <div class="display_f">
+            <div>지역</div>
+            <p>{{params.location}}</p>
+          </div>
+          <hr/>
+          <div class="display_f">
+            <div>내용</div>
+            <div class="write_widt"><p>{{params.description}}</p></div>
+          </div>
+          <hr/>
+          <div class="display_f">
+            <div>날짜</div>
+            <p>{{params.date_posted}}</p>
+          </div>
+       </div>
 </template>
 
 <script setup>
@@ -56,5 +59,15 @@ axios.get("http://127.0.0.1:8000/jobs/get/{id}",{
 </script>
 
 <style>
-
+.display_f{
+  display: flex;
+}
+.write_widt{
+  width: 150px;
+  height: 300px;
+  word-break: break-all;
+}
+.line-height{
+  line-height: 30px;
+}
 </style>
